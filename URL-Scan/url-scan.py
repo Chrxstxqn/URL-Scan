31469869f290852fb4579d4c1c7294d19c11f25e
Chrxstxqn <137430908+Chrxstxqn@users.noreply.github.com>
2023-07-10 16:58:01 +0200

    url-scan.py

--- URL-Scan/url-scan.py
+++ URL-Scan/url-scan.py
@@ -23,8 +23,10 @@ encoded_url = urllib.parse.quote(url, safe='')
 api_url = "https://ipqualityscore.com/api/json/url/6iP3Ci09b83suqDijoQ4pHSD9uayadBp/"
 data = requests.get(api_url + encoded_url)
 print(json.dumps(data.json(), indent=4))
+
 print("")
 print("")
+
 print("Tanks for using! <3")
 print("Support me on Instagram : '_chrxstxqn_' " )
 
